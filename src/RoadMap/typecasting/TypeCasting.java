62101720d6319918ec9345b1d4d98333088051ae
Josué David Martinez Ortiz <jmartinezo12@miumg.edu.gt>
2026-04-20 22:20:49 -0600

    subiendo archivos con type casting

---
+++ src/RoadMap/typecasting/TypeCasting.java
@@ -0,0 +1,23 @@
+package RoadMap.typecasting;
+
+public class TypeCasting {
+
+	
+	
+	
+
+	public TypeCasting() {
+		// Ques es el type casting es una manera de hacer que una variable cambie de tipo int a un Striing
+		
+		int num = 10;
+		  System.out.println("el numero es " + num );
+		  
+		  // convercion a un double
+		  double data = num;
+		  // double es el valor que le asignamos 
+		  //data es el nombre de la nueva variable que va a hacer de tipo double
+		  // y el num llamamos de la variable anterior y le asignamos su nnuevo valor
+		  System.out.print("el numero es " + data);
+		  
+			}
+}
